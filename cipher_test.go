749ce37a6b8572df2795b7671f711854789a8d41
godcong <jumbycc@163.com>
2019-11-08 14:05:44 +0800

    add test key

--- cipher_test.go
+++ cipher_test.go
@@ -34,7 +34,12 @@ func TestNewCipherEncoder(t *testing.T) {
 	if e != nil {
 		t.Error(e)
 	}
-	enc := NewCipherEncoder(pubKey, 4999, time.Now())
+	tm, err := time.Parse(TimeStampFormat, "20191107")
+	if err != nil {
+		return
+	}
+
+	enc := NewCipherEncoder(pubKey, 4999, tm)
 	bytes, e := enc.Encode("/ip4/127.0.0.1/tcp/14001/ipfs/QmRnCapPN73gDHRhnmKD8VbFjsqdF6Y5adzihwbjULaTcx")
 	if e != nil {
 		t.Error(e)
@@ -49,7 +54,11 @@ func TestNewCipherDecode(t *testing.T) {
 	if e != nil {
 		t.Error(e)
 	}
-	dec := NewCipherDecode(privateKey, time.Now())
+	tm, err := time.Parse(TimeStampFormat, "20191107")
+	if err != nil {
+		return
+	}
+	dec := NewCipherDecode(privateKey, tm)
 	bytes, e := dec.Decode(`Z2qARs0/+rLyV4o0LEnAVFy0otp+CQh3krVWh/hgmXJI3LCnyoWjWZO+tHrCvX7WPliAi3xv0O9G3qSWCgqR6hdmtmFpYDeubPp66i9FR5o1poOz6YJybhUW7WohVYs/R5pzv7E6pomxAoB4ifqfCJUpuZr7LIgNgb+fU0hlnDWunhE4DUQtbrR+f6Iqo4lfXq1gAZgo5WVBnHRHnf+vuTHcuKCr0WwwgNjx0KRWHjhbK7w5MzuTWJyHKu5F9uqS6zZRMtCFsNbWwNkikF/0OP/HSsrHJxshQbBHSYH6ParuN+E6c1B24/JG9r+HOrgdCFdjUU1Bv5HARLmdsUZwZb0Vk4MUmp2nKPeWbHet5oTG9VoLuLT3GmcVDJ6hHdodpj4e5AeszcLSf0sRtOSsAshYL3+MJn4nAaCmgODyIOyo+QB2C8Dn0Gve6MbD130i4jX5CyAsouvkgvjGiacH5qRydL7aBwngolc8663iVHHqotu9HEszxbSEEytlN+IAXkIySgIsHTIKWONd17qTOvvO6JfNkfPQm661LFr5cs2SO7r1ax0LNHKFEqyRDBPlWsD1C2ZWKQeRWOxXnqU/6YaRe5F6neHVTUzvF1T43G/C0OYWewfulTUgZOyiQDJabmYn8lsRUSmBx44QDLvGVH0nGscmgRDMyinazGx3lgk=`)
 	if e != nil {
 		t.Error(e)
